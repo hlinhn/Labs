9d51e34eef647479017ce181b231dc398c5498f0
halinhh <halinh.nguyen424@gmail.com>
2016-09-05 04:17:44 +0800

    Test part 1 on hardware
    Warnings about temp in module counter, most likely harmless - since I can't read from output, however, this still needs to be looked into more properly

--- lab1/lab1.srcs/sources_1/imports/EE4218/counter.vhd
+++ lab1/lab1.srcs/sources_1/imports/EE4218/counter.vhd
@@ -56,27 +56,17 @@ architecture Behavioral of counter is
           clk_div : out std_logic);
     end component;
     
-    component single_pulser is
-    port (clk : in std_logic;
-          sig_in : in std_logic;
-          hold : in std_logic;
-          slow_clk : in std_logic;
-          sig_out : out std_logic);
-    end component;
-    
     signal clk_btn : std_logic := '0';
     
-    signal dirdeb, dir_fin : std_logic := '0';    
-    signal loaddeb, load_fin : std_logic := '0';    
-    signal modedeb, mode_fin : std_logic := '0';    
-    signal tickdeb, tick_fin : std_logic := '0';
-    
-    signal dir_hold, mode_hold, load_hold, tick_hold : std_logic;
+    signal dirdeb, old_dir : std_logic := '0';    
+    signal loaddeb : std_logic := '0';    
+    signal modedeb, old_mode : std_logic := '0';    
+    signal tickdeb, old_tick : std_logic := '0';
     
     signal clk_div, old_clk : std_logic := '0';
     signal temp : std_logic_vector(3 downto 0) := "0000";
 
-    constant CLK_SIZE : integer := 4;
+    constant CLK_SIZE : integer := 5;
     constant CLK_BTN_SIZE : integer := 2;
 begin
     divider : clock_divider generic map (CLK_SIZE => CLK_SIZE)
@@ -91,42 +81,33 @@ begin
     mode_deb : debouncer generic map (N => 1)
     port map (clk => clk_btn, btn => mode, outsig => modedeb);
     tick_deb : debouncer generic map (N => 1)
-    port map (clk => clk_btn, btn => tick, outsig => tickdeb);
-    
-    tick_hold <= '0';
-    
-    pulser_dir : single_pulser 
-    port map (clk => clk_btn, sig_in => dirdeb, hold => dir_hold, slow_clk => clk_div, sig_out => dir_fin);
-    pulse_load : single_pulser 
-    port map (clk => clk_btn, sig_in => loaddeb, hold => load_hold, slow_clk => clk_div, sig_out => load_fin);
-    pulse_mode : single_pulser
-    port map (clk => clk_btn, sig_in => modedeb, hold => mode_hold, slow_clk => clk_div, sig_out => mode_fin);
-    pulse_tick : single_pulser
-    port map (clk => clk_btn, sig_in => tickdeb, hold => tick_hold, slow_clk => clk_div, sig_out => tick_fin);    
-     
---    direction_changed <= dirdeb xor old_dir;
---    mode_changed <= modedeb xor old_mode;
---    tick_changed <= tickdeb xor old_tick;    
+    port map (clk => clk_btn, btn => tick, outsig => tickdeb);           
     
     process (clk_btn, loaddeb, num)
     variable direction : std_logic := '0';
     variable mode_auto : std_logic := '1';
-    
+    variable mode_changed, dir_changed : std_logic := '0';
     begin
-        if load_fin = '1' then
+        if loaddeb = '1' then
             temp <= num;
         elsif (clk_btn'event and clk_btn = '1') then
-            if mode_auto = '1' then
-                dir_hold <= '1';
-                mode_hold <= '1';
-                load_hold <= '1';
+            if modedeb = '1' and old_mode = '0' then
+                mode_changed := '1';
+            end if;
+            if dirdeb = '1' and old_dir = '0' then
+                dir_changed := '1';
+            end if;
+            
+            if mode_auto = '1' then                    
                 if clk_div = '1' and old_clk = '0' then            
-                    if mode_fin = '1' then
+                    if mode_changed = '1' then
                         mode_auto := not mode_auto;
+                        mode_changed := '0';
                     end if;
-                    
-                    if dir_fin = '1' then
+                                        
+                    if dir_changed = '1' then
                         direction := not direction;
+                        dir_changed := '0';
                     end if;
                                         
                     if direction = '0' then 
@@ -136,20 +117,18 @@ begin
                     end if;
                 end if;
                            
-            else
-                dir_hold <= '0';
-                mode_hold <= '0';
-                load_hold <= '0';
-                
-                if mode_fin = '1' then
+            else                
+                if mode_changed = '1' then
                     mode_auto := not mode_auto;
+                    mode_changed := '0';
                 end if;
                 
-                if dir_fin = '1' then
+                if dir_changed = '1' then
                     direction := not direction;
+                    dir_changed := '0';
                 end if;
                 
-                if tick_fin = '1' then
+                if tickdeb = '1' and old_tick = '0' then
                     if direction = '0' then
                         temp <= std_logic_vector(unsigned(temp) + 1);
                     else
@@ -158,6 +137,9 @@ begin
                 end if;     
             end if;
             old_clk <= clk_div;                    
+            old_mode <= modedeb;
+            old_dir <= dirdeb;
+            old_tick <= tickdeb;
         end if;         
     end process;
     count <= temp;            
